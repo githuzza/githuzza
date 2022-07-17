c9bded11f6d38bee4a4de236f235ac8b9b571f19
githuzza <islam.huzza@gmail.com>
2022-07-17 12:03:42 -0400

    new task

---
+++ src/main/java/pack/hi.java
@@ -0,0 +1,26 @@
+package pack;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		testing();
+	}
+	public static void testing() {
+		
+		System.out.println("sup weekend");
+		
+	
+	}
+	
+	
+}
+	
+	
+	
+	
+
+
